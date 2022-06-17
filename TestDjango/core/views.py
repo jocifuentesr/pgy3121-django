34b9ce05469a7f2e11b0caaee04e7ab6a64a1910
José Cifuentes R <josecifuentesr@gmail.com>
2022-06-17 16:55:25 -0400

    3.1.2 terminado

--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+        super().__init__()
+
+
 def home(request):
-    return render(request, 'core/home.html')
+    lista = ["comer pantuflas", "pasear", "comer"]
+    responsable = Persona("Humana Claudia", "35")
+    contexto = {"nombre": "Kiara Pug", "actividades": lista, "responsable": responsable}
+    return render(request, 'core/home.html', contexto)
